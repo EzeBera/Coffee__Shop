d4f6ea3517cdcc2a72b7acd165040893394954f9
EzeBera <21325692+EzeBera@users.noreply.github.com>
2024-09-24 10:25:20 -0300

    add Footer component - add footer css - add Footer in App - change name and icon in index html

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Offer from "./components/offer/Offer";
 import Team from "./components/team/Team";
 import Reservation from "./components/reservation/Reservation";
 import Testimonials from "./components/testimonials/Testimonials";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
       <Team />
       <Reservation />
       <Testimonials />
+      <Footer />
     </>
   );
 }
